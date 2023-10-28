62c161ba76c8146b01bbf92fc43a8191d6c47b92
Guy <Guyhanandev@gmail.com>
2023-10-28 15:23:43 +0300

    platfromer move controll been add to libarary

--- Arial's Nightmares/Scripts/Library/PlayerLibrary.gd
+++ Arial's Nightmares/Scripts/Library/PlayerLibrary.gd
@@ -9,6 +9,10 @@ class_name Player_library
 @export var speed : float = 1
 var vel := Vector2.ZERO
 
+
+const JUMP_VELOCITY = -400.0
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
 func _process(delta):
 	Move2D(delta)
 	pass
@@ -28,5 +32,23 @@ func Move2D(delta) -> void:
 		$Player_Sprite.flip_h = true
 	pass
 
-func MovePlatform(delta) -> void:
+
+func PlatformMove2D(delta) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	# Handle Jump.
+	if Input.is_action_just_pressed("space") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction = Input.get_axis("Left", "Right")
+	if direction:
+		velocity.x = direction * speed * 100
+	else:
+		velocity.x = move_toward(velocity.x, 0, speed * 100)
+
+	move_and_slide()
 	pass
