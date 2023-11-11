6cca18ed71cf68d497dd5f9001fe21c7c6d7cdb1
EvlNrostA <nirfin267@gmail.com>
2023-11-11 09:14:45 +0200

    Made joystick boundary circular

--- Arial's Nightmares/Scripts/Mechanics/Joystick.gd
+++ Arial's Nightmares/Scripts/Mechanics/Joystick.gd
@@ -6,7 +6,8 @@ const MIN_OFFSET = 0.2
 
 @onready var input = $InputButton
 @onready var size = Vector2(bitmask.get_size())
-@onready var max_distance = size.x / 2
+@onready var max_distance = size / 2
+@onready var max_length = max_distance.x
 
 func _process(_delta):
 	if visible and GManager.is_mobile:
@@ -14,16 +15,20 @@ func _process(_delta):
 
 func move():
 	if is_pressed():
+		#input.position = mouse_pos.clamp(size * LIMIT_MULTIPLIER, size * (1 - LIMIT_MULTIPLIER)) - START_POS
 		var mouse_pos = get_local_mouse_position()
-		input.position = mouse_pos.clamp(size * LIMIT_MULTIPLIER, size * (1 - LIMIT_MULTIPLIER)) - START_POS
-		apply_input()
+		var normalized_pos = mouse_pos - max_distance
+		
+		var angle = atan2(normalized_pos.x, normalized_pos.y)
+		var movement = clamp(normalized_pos.length(), 0, max_length) * Vector2(sin(angle), cos(angle))
+		input.position = movement + START_POS
+
+		apply_input(movement)
 	else:
 		input.position = START_POS
 
-func apply_input():
+func apply_input(movement):
 	reset_input()
-	
-	var movement = input.position - START_POS
 	
 	"""
 	var distance = movement.distance_to(Vector2.ZERO) / max_distance
@@ -39,10 +44,10 @@ func apply_input():
 			Input.action_press("Up")
 	"""
 	
-	Input.action_press("Right", movement.distance_to(Vector2(-max_distance, 0)) / max_distance)
-	Input.action_press("Down", movement.distance_to(Vector2(0, -max_distance)) / max_distance)
-	Input.action_press("Left", movement.distance_to(Vector2(max_distance, 0)) / max_distance)
-	Input.action_press("Up", movement.distance_to(Vector2(0, max_distance)) / max_distance)
+	Input.action_press("Right", movement.distance_to(Vector2(-max_length, 0)) / max_length)
+	Input.action_press("Down", movement.distance_to(Vector2(0, -max_length)) / max_length)
+	Input.action_press("Left", movement.distance_to(Vector2(max_length, 0)) / max_length)
+	Input.action_press("Up", movement.distance_to(Vector2(0, max_length)) / max_length)
 		
 func reset_input():
 	Input.action_release("Right")
